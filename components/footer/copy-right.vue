87909ea0ccf66899476ee1ff87367b77d48325bf
wayearn <admin@wayearn.com>
2022-07-30 06:56:07 +0800

    fix: 增加icp备案链接

--- components/footer/copy-right.vue
+++ components/footer/copy-right.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center py-2 text-[16px]">
     <p>Copyright © 2018-{{ year }} 武汉为恩科技有限公司 版权所有</p>
-    <p>鄂ICP备19018123号-2</p>
+    <a href="https://beian.miit.gov.cn/" title="工业和信息化部政务服务平台">鄂ICP备19018123号-2</a>
   </div>
 </template>
 

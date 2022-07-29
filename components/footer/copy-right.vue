de61d4ef78ad0880dba62d722788045754346652
wayearn <admin@wayearn.com>
2022-07-30 07:00:29 +0800

    dept: update package.json

--- components/footer/copy-right.vue
+++ components/footer/copy-right.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center py-2 text-[16px]">
     <p>Copyright © 2018-{{ year }} 武汉为恩科技有限公司 版权所有</p>
-    <a href="https://beian.miit.gov.cn/" title="工业和信息化部政务服务平台">鄂ICP备19018123号-2</a>
+    <a href="https://beian.miit.gov.cn/" title="工业和信息化部政务服务平台" target="_blank">鄂ICP备19018123号-2</a>
   </div>
 </template>
 

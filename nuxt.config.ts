117e84bd4f419051613ad3c193ed046aa3a71b5b
wayearn <admin@wayearn.com>
2024-01-19 12:15:52 +0800

    feat: 更新企业站标题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: '科技改变生活 - 武汉为恩科技有限公司',
+    },
+  },
   css: ['~/assets/css/tailwind.css'],
   postcss: {
     plugins: {
